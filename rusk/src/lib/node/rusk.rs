fd4109b6bdb1e7447a3d930847c14bbaeebd9bb9
Milosz Muszynski <milosz.muszynski@gmail.com>
2024-09-23 10:35:19 +0200

    instrumented set_base_and_delete

--- rusk/src/lib/node/rusk.rs
+++ rusk/src/lib/node/rusk.rs
@@ -500,6 +500,9 @@ impl Rusk {
         base: [u8; 32],
         to_delete: Vec<[u8; 32]>,
     ) {
+        let del_str: Vec<String> = to_delete.iter().map(|a|hex::encode(a)).collect();
+        println!("SET_BASE_AND_DELETE base={} delete={:?}", hex::encode(base), del_str);
+
         self.tip.write().base = base.clone();
         task::spawn(finalize_commit(self.vm.clone(), base));
 
